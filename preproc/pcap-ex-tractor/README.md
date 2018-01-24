57b92fc71de249eef0fb57a81cd7cf4e08ca28f4
Ondra Lengal <ondra.lengal@gmail.com>
2018-01-24 18:07:56 +0100

    pcap-ex-tractor: add SPiCe format

--- preproc/pcap-ex-tractor/README.md
+++ preproc/pcap-ex-tractor/README.md
@@ -1,13 +1,31 @@
 PCAP-Ex-Tractor
 ===============
 Extracts payloads from packets within a PCAP file and writes them into a text
-file, one per line, of hexadecimal codes of the bytes, separated by a space,
-e.g.,
+file.  There are two text file formats to choose from.
+
+Text hexa format
+----------------
+The format contains packets, one per line, each expressed using hexadecimal
+codes of the bytes, separated by a space, e.g.,
 ```
 3A 2F 2F 77 77 77 2E 79 61 68 6F 6F 2E 63
 6F 6D 2F 65 78 74
 2F 6D 73 66 69 6E 64 66 61 73 74 2F 69 6E 64 65 78 32 2E 68 74 6D 6C 22 20 4E 41 4D 45 3D 22 72 69 67 68 74 22 20 4E 4F 52 45 53 49 5A 45 20 53 43 52 4F 4C 4C 49 4E 47 3D 4E 4F 3E 0D 0A 09 09 09 3C 46 52 41 4D 45 20 4D 41 52 47 49 4E 57 49 44 54 48 3D 22 30 22 20 4D 41 52 47 49 4E 48 45
 ```
+
+SPiCe format
+------------
+This format is used in the [SPiCe competition](http://spice.lif.univ-mrs.fr).
+The first line contains two fields: the number of samples and the size of the alphabet, separated by a whitespace.
+The body of the file has a line for every packet.
+Each line of the body starts with the length of the packet, followed by a sequence of numbers (base-10) from the range \[0, alphabet size-1\].
+Example:
+```
+2 256
+9 1 2 3 4 5 6 7 8 7
+4 42 0 255 3
+```
+
 
 Prerequisities
 ==============
@@ -29,5 +47,5 @@ Running
 =======
 Run
 ```
-./pcap-ex-tractor INPUT.pcap > OUTPUT.txt
+./pcap-ex-tractor [-t|-s] INPUT.pcap > OUTPUT.txt
 ```
--- preproc/pcap-ex-tractor/pcap-ex-tractor.cc
+++ preproc/pcap-ex-tractor/pcap-ex-tractor.cc
@@ -17,8 +17,17 @@
 
 size_t total_packets = 0;
 
+
+enum class OutputFormat {TEXT, SPICE};
+OutputFormat format = OutputFormat::TEXT;
+
 // FUNCTION DECLARATIONS
-void packetHandler(
+void final_loop(
+	u_char*             userData,
+	const pcap_pkthdr*  pkthdr,
+	const u_char*       packet);
+
+void counting_loop(
 	u_char*             userData,
 	const pcap_pkthdr*  pkthdr,
 	const u_char*       packet);
@@ -26,37 +35,65 @@ void packetHandler(
 
 void print_usage(const char* prog_name)
 {
-	std::cout << "usage: " << prog_name << " <input.pcap>\n";
+	std::cout << "usage: " << prog_name << "[-t|-s] <input.pcap>\n";
+	std::cout << "    -t   use text hexa format\n";
+	std::cout << "    -s   use SPiCe competition format\n";
 }
 
 int main(int argc, char** argv)
 {
-	if (argc != 2)
+	if (argc != 3)
 	{
 		print_usage(argv[0]);
 		return EXIT_FAILURE;
 	}
 
-	std::string packets_file = argv[1];
+	std::string str_format   = argv[1];
+	std::string packets_file = argv[2];
+	if ("-t" == str_format) {
+		format = OutputFormat::TEXT;
+	} else if ("-s" == str_format) {
+		format = OutputFormat::SPICE;
+	} else {
+		print_usage(argv[0]);
+		return EXIT_FAILURE;
+	}
 
 	// open capture file for offline processing
 	pcap_t *descr = pcap_util::pcap_open(packets_file);
 
+	if (OutputFormat::SPICE == format) {
+		std::cerr << "Initial pass: ";
+		if (pcap_loop(descr, 0, counting_loop, nullptr) < 0) {
+			std::cout << "pcap_loop() failed: " << pcap_geterr(descr);
+			return EXIT_FAILURE;
+		}
+
+		std::cerr << "\n";
+		std::cout << total_packets << " 256\n";
+		total_packets = 0;
+
+		pcap_close(descr);
+		descr = nullptr;
+		descr = pcap_util::pcap_open(packets_file);
+	}
+
+	std::cerr << "Final pass: ";
 	// start packet processing loop, just like live capture
-	if (pcap_loop(descr, 0, packetHandler, nullptr) < 0)
-	{
+	if (pcap_loop(descr, 0, final_loop, nullptr) < 0) {
 		std::cout << "pcap_loop() failed: " << pcap_geterr(descr);
 		return EXIT_FAILURE;
 	}
+	std::cerr << "\n";
 
 	return EXIT_SUCCESS;
 }
 
 
-void packetHandler(
-	u_char* /* userData */,
-	const pcap_pkthdr* pkthdr,
-	const u_char* packet)
+void final_loop(
+	u_char*             /* userData */,
+	const pcap_pkthdr*  pkthdr,
+	const u_char*       packet)
 {
 	assert(nullptr != pkthdr);
 	assert(nullptr != packet);
@@ -67,14 +104,43 @@ void packetHandler(
 
 	if (len < 0) { return; }
 
-	for (size_t i = 0; i < static_cast<size_t>(len); ++i)
-	{
-		if (i != 0) { std::cout << " "; }
-		std::cout << std::hex << std::setw(2) << std::setfill('0') <<
-			std::uppercase << static_cast<unsigned>(payload[i]);
+	if (OutputFormat::SPICE == format) {
+		std::cout << len;
+
+		for (size_t i = 0; i < static_cast<size_t>(len); ++i) {
+			std::cout << " ";
+			std::cout << static_cast<unsigned>(payload[i]);
+		}
+	} else {
+		assert(OutputFormat::TEXT == format);
+		for (size_t i = 0; i < static_cast<size_t>(len); ++i) {
+			if (i != 0) { std::cout << " "; }
+			std::cout << std::hex << std::setw(2) << std::setfill('0') <<
+				std::uppercase << static_cast<unsigned>(payload[i]);
+		}
 	}
 
 	std::cout << "\n";
+
+	++total_packets;
+	if (total_packets % 1000 == 0)
+	{
+		std::cerr << "#";
+		std::cerr.flush();
+	}
+}
+
+void counting_loop(
+	u_char*             /* userData */,
+	const pcap_pkthdr*  pkthdr,
+	const u_char*       packet)
+{
+	assert(nullptr != pkthdr);
+	assert(nullptr != packet);
+
+	int len;
+	std::tie(std::ignore, len) = pcap_util::get_payload(pkthdr, packet);
+	if (len < 0) { return; }
 
 	++total_packets;
 	if (total_packets % 1000 == 0)
